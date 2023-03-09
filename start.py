5f55d51184e4d529b9150037a9ba2d2f93e795f8
lukewain <lukewainhouse@outlook.com>
2023-03-09 18:14:58 +0000

    Reformat code

--- start.py
+++ start.py
@@ -4,15 +4,15 @@ import asyncpg
 import os
 import dotenv
 
-from dotenv import load_dotenv; load_dotenv()
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DSN = f"postgres://{os.environ['DBUSER']}:{os.environ['DBPASS']}@192.168.1.124/{os.environ['DBNAME']}"
 
+
 async def run():
-    async with (
-        asyncpg.create_pool(DSN) as pool,
-        MEE6Slander(pool=pool) as bot
-    ):
+    async with (asyncpg.pool.create_pool(DSN) as pool, MEE6Slander(pool=pool) as bot):
         await bot.start(bot.token)
 
 
