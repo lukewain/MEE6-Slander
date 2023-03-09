8db9ddb02e550ad42ca93f6184b59dab51773b55
lukewain <lukewainhouse@outlook.com>
2023-03-09 18:19:11 +0000

    Remove unncesary code and update links

--- src/bot.py
+++ src/bot.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 
 class MEE6Slander(Bot):
-    def __init__(self, *, pool: asyncpg.Pool):
+    def __init__(self, *, pool: asyncpg.pool.Pool):
         # Define the bot's intents
         intents = Intents().default()
         intents.message_content = True
@@ -27,22 +27,14 @@ class MEE6Slander(Bot):
         allowed_mentions = AllowedMentions(everyone=False, users=False, roles=False)
 
         super().__init__(
-            command_prefix="s.", intents=intents, allowed_mentions=allowed_mentions
+            command_prefix="sbeta.", intents=intents, allowed_mentions=allowed_mentions
         )
 
         setup_logging(level=INFO)
 
         self.token = environ["TOKEN"]
 
-        self.github: str = "https://github.com/mee6-slander"
-        try:
-            self.pool = create_pool(
-                user=environ["DB_USER"],
-                host=environ["DB_HOST"],
-                password=environ["DB_PASSWORD"],
-            )
-        except Exception as e:
-            utils.log(e, "ERROR")
+        self.github: str = "https://github.com/MEE6-Slander"
 
         self.dirList = []
 
@@ -51,14 +43,12 @@ class MEE6Slander(Bot):
         with open("./config/admin.json") as adminfile:
             t = load(adminfile)
 
-        for a in t['admins']:
+        for a in t["admins"]:
             if a not in self.admins:
                 self.admins.append(a)
 
-
         # Create the asyncpg pool connection
-        self.pool = pool
-
+        self.pool: asyncpg.pool.Pool = pool
 
     async def setup_hook(self) -> None:
         await self.load_extension("jishaku")
@@ -67,6 +57,9 @@ class MEE6Slander(Bot):
         with open("total_slander.json") as f:
             data = load(f)
             self.total = data["total"]
+
+        await utils.parse_schema("./src/schema.sql", self.pool)
+        utils.log("Schem successfully uploaded")
 
     async def on_ready(self) -> None:
         utils.log(f"Logged in as {self.user}")
