f196abea40f413c15e1b73a68cacc2bb577e31ff
lukewain <lukewainhouse@outlook.com>
2023-03-09 18:19:18 +0000

    Fix schema error

--- src/schema.sql
+++ src/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS guilds (
 );
 
 CREATE TABLE IF NOT EXISTS admins (
-    id BIGINT PRIMARY KEY
+    id BIGINT PRIMARY KEY,
     added BIGINT,
     add_others BOOLEAN,
     added_by BIGINT
