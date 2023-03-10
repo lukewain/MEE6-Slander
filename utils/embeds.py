229e970c4dbee2603d360d44220ed0a89bda1081
lukewain <lukewainhouse@outlook.com>
2023-03-10 22:14:16 +0000

    Set embeds to be staticmethods and added stats embed

--- utils/embeds.py
+++ utils/embeds.py
@@ -4,27 +4,38 @@ from discord.utils import utcnow
 
 
 class MEE6Embed(Embed):
+    @staticmethod
     def info(title: str, description: str):
         embed = Embed(description=description)
         embed.set_author(name=title, icon_url=Emoji.info.url)
         return embed
 
+    @staticmethod
     def error(title: str, description: str):
         embed = Embed(description=description)
         embed.set_author(name=title, icon_url=Emoji.error.url)
         return embed
 
+    @staticmethod
     def warn(title: str, description: str):
         embed = Embed(description=description)
         embed.set_author(name=title, icon_url=Emoji.warn.url)
         return embed
 
+    @staticmethod
     def success(title: str, description: str):
         embed = Embed(description=description)
         embed.set_author(name=title, icon_url=Emoji.success.url)
         return embed
 
-    def server_join(guild: Guild, server_count: int, total_members: int, added_members: int, has_mee6: bool):
+    @staticmethod
+    def server_join(
+        guild: Guild,
+        server_count: int,
+        total_members: int,
+        added_members: int,
+        has_mee6: bool,
+    ):
         embed = Embed(title=f"Joined Server!", colour=Color.green())
         embed.add_field(name="Server Name", value=f"{guild.name}", inline=False)
         embed.add_field(name="Total Servers", value=f"{server_count}")
@@ -32,11 +43,17 @@ class MEE6Embed(Embed):
         embed.add_field(name="Server ID", value=f"{guild.id}")
         embed.add_field(name="Total Members", value=f"{total_members}", inline=False)
         embed.add_field(name="Added Members", value=f"+{added_members}")
-        embed.add_field(name="Has MEE6", value=f"```diff\n{'+ Has MEE6' if has_mee6 else '- Does not have MEE6'}\n```")
+        embed.add_field(
+            name="Has MEE6",
+            value=f"```diff\n{'+ Has MEE6' if has_mee6 else '- Does not have MEE6'}\n```",
+        )
         embed.set_footer(text=utcnow())
         return embed
 
-    def server_leave(guild: Guild, server_count: int, total_members: int, removed_members: int):
+    @staticmethod
+    def server_leave(
+        guild: Guild, server_count: int, total_members: int, removed_members: int
+    ):
         embed = Embed(title=f"Left Server!", colour=Color.red())
         embed.add_field(name="Server Name", value=f"{guild.name}", inline=False)
         embed.add_field(name="Total Servers", value=f"{server_count}")
@@ -46,8 +63,25 @@ class MEE6Embed(Embed):
         embed.add_field(name="Removed Members", value=f"-{removed_members}")
         embed.set_footer(text=utcnow())
         return embed
-    
+
+    @staticmethod
     def mee6_not_found(owner: Member):
-        embed = Embed(description="Woah, MEE6 isn't in your server\nThis means that I will not work correctly!\nPlease invite MEE6 using https://MEE6.xyz")
+        embed = Embed(
+            description="Woah, MEE6 isn't in your server\nThis means that I will not work correctly!\nPlease invite MEE6 using https://MEE6.xyz"
+        )
         embed.set_author(name="MEE6 not found!", icon_url=Emoji.warn.url)
+        return embed
+
+    @staticmethod
+    def stats_embed(sent, deleted, edited, joined, left, slandered, started, icon_url):
+        embed = Embed(
+            description=f"Stats from the bot since <t:{started}:F>", color=Color.gold()
+        )
+        embed.set_author(name="Stats", icon_url=icon_url)
+        embed.add_field(name="Messages Seen", value=sent, inline=False)
+        embed.add_field(name="Messages Deleted", value=deleted, inline=False)
+        embed.add_field(name="Messages Edited", value=edited, inline=False)
+        embed.add_field(name="Servers Joined", value=joined, inline=False)
+        embed.add_field(name="Servers Left", value=left, inline=False)
+        embed.add_field(name="Times Slandered", value=slandered, inline=False)
         return embed
