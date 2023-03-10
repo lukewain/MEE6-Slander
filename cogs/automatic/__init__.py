e10f3ac01f2a21568043359e88df409128d62dca
lukewain <lukewainhouse@outlook.com>
2023-03-10 22:18:55 +0000

    Removed unused files

--- cogs/automatic/__init__.py
+++
@@ -1,5 +0,0 @@
-from cogs.automatic.fileListener import FileWatcher
-
-
-async def setup(bot):
-    await bot.add_cog(FileWatcher(bot))
--- cogs/automatic/fileListener.py
+++
@@ -1,9 +0,0 @@
-from cogs.automatic._base import AutoBase
-from discord.ext import tasks
-
-
-class FileWatcher(AutoBase):
-    @tasks.loop(minutes=60)
-    async def file_mon(self):
-        for dir in self.bot.dirList:
-            dir.updateFiles()
