c6cfa9dbfb61241923af4349cf6701007903c1bb
Leonardo Cavenago <leonardocavenago05@gmail.com>
2023-03-17 13:25:34 +0100

    Change startup sequence to have a slander manager and pool

--- src/bot.py
+++ src/bot.py
@@ -1,23 +1,24 @@
+from __future__ import annotations
+
 from discord.ext.commands import Bot
 from discord import Intents, AllowedMentions, Status, Game
 from discord.utils import setup_logging
 
-from logging import INFO
-from asyncpg import create_pool
-from os import environ, getcwd
+from os import environ
 from dotenv import load_dotenv
 import asyncpg
 
-from json import load, dump
+from json import load
 
 
 import utils
 
 load_dotenv()
+setup_logging()
 
 
 class MEE6Slander(Bot):
-    def __init__(self, *, dsn: str):
+    def __init__(self, *, pool: asyncpg.Pool[asyncpg.Record], slander_manager: utils.SlanderManager):
         # Define the bot's intents
         intents = Intents().default()
         intents.message_content = True
@@ -25,17 +26,16 @@ class MEE6Slander(Bot):
 
         allowed_mentions = AllowedMentions(everyone=False, users=False, roles=False)
 
-        super().__init__(
-            command_prefix="s.", intents=intents, allowed_mentions=allowed_mentions
-        )
-
-        setup_logging(level=INFO)
+        super().__init__(command_prefix="s.", intents=intents, allowed_mentions=allowed_mentions)
 
         self.token = environ["TOKEN"]
 
         self.github: str = "https://github.com/MEE6-Slander"
 
         self.admins: list = []
+
+        self.pool = pool
+        self.slander_manager = slander_manager
 
         with open("./config/admin.json") as adminfile:
             t = load(adminfile)
@@ -44,21 +44,18 @@ class MEE6Slander(Bot):
             if a not in self.admins:
                 self.admins.append(a)
 
-        # Register postgres dsn 
-        self.dsn: str = dsn
-        self.default_timeout: int = 300
-
     async def setup_hook(self) -> None:
         await self.load_extension("jishaku")
         await self.load_extension("cogs.events")
 
+    async def create_tables(self):
         with open("total_slander.json") as f:
             data = load(f)
             self.total = data["total"]
 
-        conn = await asyncpg.connection.connect(dsn=self.dsn)
-        await conn.execute("".join(open("./src/schema.sql").read()))
-        utils.log("Schem successfully uploaded")
+        with open("./src/schema.sql") as file:
+            await self.pool.execute(file.read())
+        utils.log("Schema successfully uploaded")
 
     async def on_ready(self) -> None:
         utils.log(f"Logged in as {self.user}")
--- start.py
+++ start.py
@@ -1,18 +1,26 @@
-from src.bot import MEE6Slander
-import asyncio
-import asyncpg
 import os
-import dotenv
+import asyncio
 
+import asyncpg
 from dotenv import load_dotenv
+
+from src.bot import MEE6Slander
+from utils import SlanderManager
 
 load_dotenv()
 
+channel_id = int(os.environ['QUEUE_CHID'])
+anyone_can_click = os.environ['ANYONE_CAN_CLICK'] == 'True'
 
 
 async def run():
-    async with MEE6Slander(dsn=os.environ['PG_DSN']) as bot:
-        await bot.start(bot.token)
+    async with asyncpg.create_pool(os.environ['PG_DSN']) as pool:
+        manager = SlanderManager(pool=pool, queue_channel_id=channel_id, anyone_can_click=anyone_can_click)
+        async with MEE6Slander(pool=pool, slander_manager=manager) as bot:
+            await bot.create_tables()
+            await manager.start()
+            await manager.register_views(bot)
+            await bot.start(bot.token)
 
 
 asyncio.run(run())
