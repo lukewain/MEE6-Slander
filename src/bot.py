8219b63be6f7e06d58980439f939428883445e7b
lukewain <lukewainhouse@outlook.com>
2023-03-18 19:30:40 +0000

    Added webhooks and aiohttp

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 import os
+import aiohttp
 
+import discord
 from discord.ext.commands import Bot
-from discord import Intents, AllowedMentions, Status, Game
+from discord import DiscordException, Intents, AllowedMentions, Status, Game
 from discord.utils import setup_logging
 
 from os import environ
@@ -20,32 +22,37 @@ setup_logging()
 
 
 class MEE6Slander(Bot):
-    def __init__(self, *, pool: asyncpg.Pool[asyncpg.Record], slander_manager: utils.SlanderManager, dev_mode: bool):
+    def __init__(self, *, pool: asyncpg.Pool[asyncpg.Record], slander_manager: utils.SlanderManager, aiosession: aiohttp.ClientSession, dev_mode: bool):
         # Define the bot's intents
-        intents = Intents().default()
+        intents: Intents = Intents().default()
         intents.message_content = True
         intents.members = True
 
+        self.prefix: str = environ['PREFIX'] if not dev_mode else environ['DEV_PREFIX']
+
         allowed_mentions = AllowedMentions(everyone=False, users=False, roles=False)
 
-        super().__init__(command_prefix="s.", intents=intents, allowed_mentions=allowed_mentions, tree_cls=SlanderTree)
+        super().__init__(command_prefix=self.prefix, intents=intents, allowed_mentions=allowed_mentions, tree_cls=SlanderTree)
 
-        self.dev_mode = dev_mode
-        self.token = environ["TOKEN"] if not dev_mode else environ['DEV_TOKEN']
+        self.dev_mode: bool = dev_mode # type: ignore
+        self.token: str = environ["TOKEN"] if not dev_mode else environ['DEV_TOKEN']
 
         self.github: str = "https://github.com/lukewain/MEE6-Slander"
 
         self.support_link: str = "https://discord.gg/EQpxMZSFy3"
-        self.show_support_link = os.environ["SHOW_SUPPORT_LINK"]
+        self.show_support_link: str = os.environ["SHOW_SUPPORT_LINK"]
 
-        self.pool = pool
-        self.slander_manager = slander_manager
+        self.pool: asyncpg.Pool[asyncpg.Record] = pool
+        self.slander_manager: utils.SlanderManager = slander_manager
+        self._session: aiohttp.ClientSession = aiosession
 
     async def setup_hook(self) -> None:
         await self.load_extension("jishaku")
         await self.load_extension("cogs.events")
         await self.load_extension("cogs.config")
         await self.load_extension("cogs.suggest")
+
+        self._log_webhook: discord.Webhook = discord.Webhook.from_url(url=os.environ['WEBHOOK_URL'], session=self._session, bot_token=self.token)
 
     async def create_tables(self):
         with open("total_slander.json") as f:
--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import aiohttp
 
 import asyncpg
 from dotenv import load_dotenv
@@ -11,12 +12,13 @@ load_dotenv()
 
 channel_id = int(os.environ['QUEUE_CHID'])
 anyone_can_click = os.environ['ANYONE_CAN_CLICK'] == 'True'
+dev_mode = os.environ['DEV'] == 'True'
 
 
 async def run():
-    async with asyncpg.create_pool(os.environ['PG_DSN']) as pool:
+    async with asyncpg.create_pool(os.environ['PG_DSN']) as pool, aiohttp.ClientSession() as session:
         manager = SlanderManager(pool=pool, queue_channel_id=channel_id, anyone_can_click=anyone_can_click)
-        async with MEE6Slander(pool=pool, slander_manager=manager) as bot:
+        async with MEE6Slander(pool=pool, slander_manager=manager, aiosession=session, dev_mode=dev_mode) as bot:
             await bot.create_tables()
             await manager.start()
             await manager.register_views(bot)
