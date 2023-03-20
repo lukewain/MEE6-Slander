179a6f6eb74b209e2c0e19a5be7a05f4f0948046
lukewain <lukewainhouse@outlook.com>
2023-03-20 18:35:13 +0000

    Updated SQL error

--- cogs/suggest.py
+++ cogs/suggest.py
@@ -72,7 +72,7 @@ class Suggestions(commands.Cog):
                     await self.bot._log_webhook.send(embed=discord.Embed(title="User DM Error", description=e, colour=discord.Colour.red()))
 
             # Update the record to say that the user has been notified
-            query: str = "UPDATE slander SET notified=True WHERE id=$1"
+            query: str = "UPDATE slanders SET notified=True WHERE id=$1"
 
             await self.bot.pool.execute(query, record['id'])
 
