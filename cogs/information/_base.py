33d9251e26b2c5f16cc2b6af77f883bbe4bd0167
lukewain <lukewainhouse@outlook.com>
2023-03-20 16:16:29 +0000

    Fix check issue

--- cogs/information/_base.py
+++ cogs/information/_base.py
@@ -11,15 +11,15 @@ class Information(commands.Cog):
         res: asyncpg.Record | None = await interaction.client.pool.fetchrow("SELECT * FROM blacklist WHERE id=$1", interaction.user.id)
 
         if not res:
-            return await interaction.response.send_message(embed=discord.Embed(title="Whoops", description="You do not have permission to do that!"))
+            return True
         
-        return False
+        return await interaction.response.send_message(embed=discord.Embed(title="Whoops", description="You do not have permission to do that!"))
         
     async def cog_check(self, ctx: commands.Context[MEE6Slander]):
         # Check if the user is in the blacklist db
         res: asyncpg.Record | None = await ctx.bot.pool.fetchrow("SELECT * FROM blacklist WHERE id=$1", ctx.author.id)
 
         if not res:
-            return await ctx.send(embed=discord.Embed(title="Whoops", description="You do not have permission to do that!"))
+            return True
         
-        return False
+        return await ctx.send(embed=discord.Embed(title="Whoops", description="You do not have permission to do that!"))
