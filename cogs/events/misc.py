0b581b3df486cc8fb09ecdd29116e2f96bacd3b9
Luke Wainhouse <lukewainhouse@outlook.com>
2023-08-28 15:28:15 +0100

    Updated to use webhooks

--- cogs/events/misc.py
+++ cogs/events/misc.py
@@ -10,19 +10,21 @@ class MemberJoinLeave(Events):
     @Cog.listener()
     async def on_member_join(self, member: discord.Member):
         await self.bot.join_leave_webhook.send(
+            usename="MEE6 Slander Join",
+            avatar_url=self.bot.user.display_avatar.url,
             embed=discord.Embed(title="Member Joined!", colour=discord.Colour.green())
             .add_field(name="User Name", value=member, inline=False)
             .add_field(name="Server Name", value=member.guild.name, inline=False)
-            .add_field(name="Member ID", value=member.id, inline=False)
+            .add_field(name="Member ID", value=member.id, inline=False),
         )
 
     @Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        channel: discord.TextChannel = await self.bot.fetch_channel(utils.constants.JOIN_LEAVE_LOG_CHANNEL)  # type: ignore
-
-        await channel.send(
+        await self.bot.join_leave_webhook.send(
+            usename="MEE6 Slander Leave",
+            avatar_url=self.bot.user.display_avatar.url,
             embed=discord.Embed(title="Member Left!", colour=discord.Colour.red())
             .add_field(name="User Name", value=member, inline=False)
             .add_field(name="Server Name", value=member.guild.name, inline=False)
-            .add_field(name="Member ID", value=member.id, inline=False)
+            .add_field(name="Member ID", value=member.id, inline=False),
         )
